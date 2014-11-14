2ac8d086a2799e7e4a36638d73d8b63cfa2ed2ea
Gordan Sajević <gsajevic@hotmail.com>
2014-11-14 19:34:58 +0100

    Lab5
    Zadaci

---
+++ korijen.java
@@ -0,0 +1,11 @@
+package lab5;
+
+public class korijen {
+	public static void main(String[] args) {
+
+		double broj=67;
+
+		System.out.println("Korijen broja " + broj + " je " + Math.sqrt(broj));
+
+	}
+}
