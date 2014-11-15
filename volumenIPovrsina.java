e1fbefee244460907ab0b08f94cfde1e62fa5a6d
Gordan Sajević <gsajevic@hotmail.com>
2014-11-15 09:20:08 +0100

    Update volumenIPovrsina.java

--- volumenIPovrsina.java
+++ volumenIPovrsina.java
@@ -4,14 +4,17 @@ public class volumenIPovrsina {
 	public static void main(String args[]){
 		double broj1=Math.random()*51;
 		double broj2=Math.random()*51;
-		System.out.println("Prvi nasumicni broj je: " + (int) broj1);
-		System.out.println("Drugi nasumicni broj je: " + (int) broj2);
-		double volumenPiramide=((int)broj1*(int)broj1*(int)broj2)/3;
-		double stranicaPiramide=(broj1*Math.sqrt((int)broj1*(int)broj1-((int)broj1*(int)broj1))/4)/2;
-		double povrsinaPiramide=(int)broj1*(int)broj1+4*stranicaPiramide;
+		int broj1Int=(int) broj1;
+		int broj2Int=(int) broj2;
+		System.out.println("Prvi nasumicni broj je: " + broj1Int);
+		System.out.println("Drugi nasumicni broj je: " + broj2Int);
+		double volumenPiramide=((int)broj1*(int)broj1*broj2Int)/3;
+		double stranicaPiramide=(broj1*Math.sqrt(broj1Int*broj1Int-(broj1Int*broj1Int))/4)/2;
+		double povrsinaPiramide=broj1Int*broj1Int+4*stranicaPiramide;
 		System.out.println("Volumen piramide je: " + volumenPiramide + 
 				", a povrsina svih stranica i baze piramide je: " + povrsinaPiramide);
-		double volumenSfere = (4*(int)broj1*(int)broj1*(int)broj1*Math.PI)/4;
-		double povrsinaSfere = 4*(int)broj1*(int)broj1*Math.PI;
+		double volumenSfere = (4*broj1Int*broj1Int*broj1Int*Math.PI)/4;
+		double povrsinaSfere = 4*broj1Int*broj1Int*Math.PI;
+		System.out.println.("Volumen Sfere je :" + volumenSfere + ", a povrsina sfere je: " + povrsinaSfere);
 	}
 }
