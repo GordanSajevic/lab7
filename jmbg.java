b904f96b641ea7c277bb462ea0439a8483408d7b
Gordan Sajević <gsajevic@hotmail.com>
2014-11-17 15:44:57 +0100

    Create jmbg.java

---
+++ jmbg.java
@@ -0,0 +1,26 @@
+package projekat;
+
+public class jmbg {
+	
+	public static void main(String[] args) {
+	String jmbg="1608990190145";
+	String danRodjenja=jmbg.substring(0,2);
+	String mjesecRodjenja=jmbg.substring(2, 4);
+	String godinaRodjenja=jmbg.substring(4, 7);
+	String regija=jmbg.substring(7, 9);
+	String spol=jmbg.substring(9,11);
+	int intDanRodjenja=Integer.parseInt(danRodjenja);
+	int intMjesecRodjenja=Integer.parseInt(mjesecRodjenja);
+	int intGodinaRodjenja=Integer.parseInt(godinaRodjenja);
+	int intRegija=Integer.parseInt(regija);
+	int intSpol=Integer.parseInt(spol);
+	if ((intDanRodjenja<=31)&&(intDanRodjenja>0)&&(intMjesecRodjenja<=12)&&(intMjesecRodjenja>0)&&(jmbg.length()<=13))
+			{
+				System.out.println("Maticni broj je validan.");
+			}
+	else
+		{
+			System.out.println("Maticni broj nije validan.");
+		}
+	}
+}
